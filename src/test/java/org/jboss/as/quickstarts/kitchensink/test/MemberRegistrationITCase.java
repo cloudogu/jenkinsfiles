c46183c35904f66d3d032232bc74a36f75cf90fe
schnatterer <johannes@schnatterer.info>
2017-07-17 01:18:15 +0200

    Made MemberRegistrationTest an integration test.
    It's run on a server. How can that be a UNIT test?

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationITCase.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationITCase.java
@@ -16,12 +16,6 @@
  */
 package org.jboss.as.quickstarts.kitchensink.test;
 
-import static org.junit.Assert.assertNotNull;
-
-import java.util.logging.Logger;
-
-import javax.inject.Inject;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.as.quickstarts.kitchensink.model.Member;
@@ -34,8 +28,13 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.inject.Inject;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(Arquillian.class)
-public class MemberRegistrationTest {
+public class MemberRegistrationITCase {
     @Deployment
     public static Archive<?> createTestArchive() {
         return ShrinkWrap.create(WebArchive.class, "test.war")
